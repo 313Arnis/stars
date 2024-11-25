9da60d31764893c1b5edef96a0aed2548baea48c
ipa23.a.razuvanovs <ipa23.a.razuvanovs@pjvt.local>
2024-11-25 13:46:19 +0200

    izveidoju submit logu

--- index.php
+++ index.php
@@ -4,3 +4,9 @@ $sql  = "SELECT * FROM users";
 $results = $conn -> query($sql);
 $row = $results->fetch_assoc();
 var_dump ($row);
+
+echo "<form>";
+    echo "<input name='username' />";
+    echo "<input type='password' name='password'/>"; 
+    echo "<input type='submit' value='Registreties>";
+echo "</form>";
