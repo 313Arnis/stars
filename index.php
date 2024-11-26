2451afb4088968cd2f5048d78ef8d7b861f52835
ipa23.a.razuvanovs <ipa23.a.razuvanovs@pjvt.local>
2024-11-26 10:27:41 +0200

    Info iet uz db

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php
 $conn = new mysqli("localhost", "root","", "stars");
-$sql  = "SELECT * FROM users";
-$results = $conn -> query($sql);
+$results = $conn -> query("SELECT * FROM users");
 $row = $results->fetch_assoc();
 var_dump ($row);
 
-echo "<form>";
-    echo "<input name='username' />";
-    echo "<input type='password' name='password'/>"; 
-    echo "<input type='submit' value='Registreties>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    
+    $sql = "INSERT INTO users (username, pass) VALUES ('$username', '$password')";
+
+    $conn -> query($sql);
+}
+
+
+
+
+
+echo "<form method = 'POST' >";
+    echo "<label> Lietotājvārds: <input type='text' name='username' /></label><br>"; 
+    echo "<label>Parole: <input type='password' name='password' /></label><br>";
+    echo "<input type='submit' value = 'registreties'>";
 echo "</form>";
+
